b92f9391f16c1b97fdddac7df23b66a7d6dd884a
Noam Aybes <noam@cleanple.com>
2023-05-01 11:28:49 +0300

    Revert + Explanation

--- models/Player.py
+++ models/Player.py
@@ -2,9 +2,8 @@ import datetime
 import random
 from models.Cards import Deck
 
-deck = Deck()
-class Player:
 
+class Player:
     def __init__(self, name: str, last_poop: datetime.datetime, player_id: int):
         self.name = name
         self.id = player_id
@@ -12,8 +11,8 @@ class Player:
         self.ap = 0
         self.last_poop = last_poop
 
-    def draw_card_to_hand(self, card):  # should be implemented in game logic instead imo
-        self.hand.append(random.choice(deck.white_cards))
+    def draw_card_to_hand(self, card):  # should be implemented in game logic instead imo --> THATS EXACTLY WHY THE LOGIC WAS JUST TO RECEIVE A CARD.
+        self.hand.append(card)
 
     def calc_AP(self, name):
         a = 0  # not sure what to do here yet
